87be093e5003b72fff196c9a7c5fb6285c687135
tkgw <takagiwa+github@gmail.com>
2020-09-13 12:43:10 +0900

    fix updates

--- adamod/adamod.py
+++ adamod/adamod.py
@@ -82,23 +82,18 @@ class AdaMod(Optimizer):
                 beta1, beta2 = group['betas']
 
                 state['step'] += 1
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
 
                 # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(grad, alpha=(1 - beta1))
                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=(1 - beta2))
 
-                denom = exp_avg_sq.sqrt().add_(group['eps'])
-
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
-
                 # Applies momental bounds on actual learning rates
-                step_size = torch.full_like(denom, fill_value=step_size)
-                step_size.div_(denom)
+                step_size = (group['lr'] / (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps']))
                 exp_avg_lr.mul_(group['beta3']).add_(step_size, alpha=(1 - group['beta3']))
                 torch.min(step_size, exp_avg_lr, out=step_size)
-                step_size.mul_(exp_avg)
+                step_size.mul_(exp_avg / bias_correction1)
 
                 if group['weight_decay'] != 0:
                     step_size.add_(p.data, alpha=group['weight_decay'] * group['lr'])
